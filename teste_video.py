331ebff129ead4730edcaf46bf5b53a3321f2790
Izairton Oliveira de Vasconcelos <iovascon@gmail.com>
2024-09-26 19:46:42 -0300

    "Atualização Arquivo teste_video.py"

--- teste_video.py
+++ teste_video.py
@@ -1,7 +1,7 @@
 import os
 
 # Verificar se o arquivo de vídeo existe no diretório especificado
-video_path = "L:/VSCode/ARDUINO/Sensores_Inteligentes/data/videos/input_video.mp4"
+video_path = "L:\VSCode\PYTHON\DIO\IoT_Sensores_Inteligentes/data/videos/input_video.mp4"
 if os.path.exists(video_path):
     print("O arquivo de vídeo existe.")
 else:
